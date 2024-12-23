43570d7c5e4b6ab998bee0207bbd3a2aa2a0cbbe
Kush Patel <kushpatel76@yahoo.com>
2024-12-23 01:27:08 -0500

    Gradient and hover changes

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,13 +14,13 @@ const NavBar = () => {
   return (
     <nav className="order-2 md:order-3 h-auto flex flex-col justify-center items-center space-y-4 rounded-lg p-8 bg-glass">
       <div className="flex flex-col items-center space-y-4">
-        <Link to="/about" className={`${getLinkClass('/about')} text-white font-bold hover:text-blue-400 transition-colors duration-300 font-sans text-2xl`}>
+        <Link to="/about" className={`${getLinkClass('/about')} text-white font-bold font-sans text-2xl`}>
           About Me
         </Link>
-        <Link to="/projects" className={`${getLinkClass('/projects')} text-white font-bold hover:text-blue-400 transition-colors duration-300 font-sans text-2xl`}>
+        <Link to="/projects" className={`${getLinkClass('/projects')} text-white font-bold font-sans text-2xl`}>
           Projects
         </Link>
-        <Link to="/contact" className={`${getLinkClass('/contact')} text-white font-bold hover:text-blue-400 transition-colors duration-300 font-sans text-2xl`}>
+        <Link to="/contact" className={`${getLinkClass('/contact')} text-white font-bold font-sans text-2xl`}>
           Contact Me
         </Link>
       </div>
