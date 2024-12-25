b36a666bbf3f2e70b0461f00e6f522be589d6c1f
Kush Patel <kushpatel76@yahoo.com>
2024-12-25 14:40:54 -0500

    typos lol

--- src/helpers/kPatelFacts.ts
+++ src/helpers/kPatelFacts.ts
@@ -1,12 +1,12 @@
 export const personalQuotes: string[] = [
-    '"Third yaer Computer Sciecne Student at Torotno Metroplitan University"',
+    '"Third-year Computer Science Student at Toronto Metropolitan University,"',
     '"Backend Software Developer at Kijiji"',
-    '"Full Stack Developer in the making"',
+    '"Full Stack Developer in the making,"',
     '"Java Demon 😈"',
-    '"Prety good at Python, C, Javascript, Typescript, and SQL. Better with Java"',
-    '"MASSIVE gamer 🎮"',
-    '"Plat 2 in Val, nothing good but I try 😅"',
+    '"Pretty good at Python, C, Javascript, Typescript, and SQL. Better with Java"',
+    '"MASSIVE gamer 🎮,"',
+    '"Plat 2 in Val, nothing good, but I try 😅"',
     '"Fortnite PRO 😎"',
-    '"I love to code, but I also love to play games"',
-    '"BIg fan of all things basketball 🏀"',
-  ];
+    '"I love to code, but I also love to play games."',
+    '"Big fan of all things basketball 🏀,"'
+];
