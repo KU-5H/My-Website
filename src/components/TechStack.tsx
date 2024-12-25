17930d49ed21550f1c15ce717ae366e538d158f9
Kush Patel <kushpatel76@yahoo.com>
2024-12-25 14:38:48 -0500

    Add personal quotes when on the about page, use other quotes when on the homepage (To be made)

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -1,36 +1,45 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import { useLocation } from 'react-router-dom';
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import { quotes } from '../helpers/quotes';
+import { personalQuotes } from '../helpers/kPatelFacts';
 import test from 'node:test';
 
 const getRandomQuote = () => {
   return quotes[Math.floor(Math.random() * quotes.length)];
 };
 
+const getRandomPersonalQuote = () => {
+  return personalQuotes[Math.floor(Math.random() * personalQuotes.length)];
+};
+
 const TechStack = () => {
   const location = useLocation();
   const [randomQuote, setRandomQuote] = useState(getRandomQuote());
+  const [randomPersonalQuote, setRandomPersonalQuote] = useState(getRandomPersonalQuote())
 
-  useEffect(() => {
-    setRandomQuote(getRandomQuote());
-  }, [location.pathname]);
+  
 
   const [text] = useTypewriter({
-    words: [randomQuote],
+    words: [location.pathname === '/' ? randomQuote : randomPersonalQuote],
     loop: 0,
     typeSpeed: 50,
     deleteSpeed: 50,
     delaySpeed: 2000,
     onDelete: () => {
-      setRandomQuote(getRandomQuote());
+      if (location.pathname === '/') {
+        setRandomQuote(getRandomQuote());
+      } else if (location.pathname === '/about') {
+        setRandomPersonalQuote(getRandomPersonalQuote());
+      }
     }
   });
 
   return (
-    <div id="Tech Stack" className="order-5 md:order-5 col-span-1 md:col-span-3 p-4 w-full rounded-lg bg-glass mt-4">
-      {location.pathname === '/about' ? (
-        <div className="text-md md:text-xl font-bold text-center whitespace-pre-wrap break-words max-w-4xl mx-auto">
+    <div id="Tech Stack" className="order-5 md:order-5 col-span-1 md:col-span-3 p-8 w-full rounded-lg bg-glass mt-4">
+
+      {location.pathname === '/about' || location.pathname === '/' ? (
+        <div className="text-md md:text-2xl font-bold text-center whitespace-pre-wrap break-words max-w-4xl mx-auto">
           <span>{text}</span>
           <Cursor />
         </div>
---
+++ src/helpers/kPatelFacts.ts
@@ -0,0 +1,12 @@
+export const personalQuotes: string[] = [
+    '"Third yaer Computer Sciecne Student at Torotno Metroplitan University"',
+    '"Backend Software Developer at Kijiji"',
+    '"Full Stack Developer in the making"',
+    '"Java Demon 😈"',
+    '"Prety good at Python, C, Javascript, Typescript, and SQL. Better with Java"',
+    '"MASSIVE gamer 🎮"',
+    '"Plat 2 in Val, nothing good but I try 😅"',
+    '"Fortnite PRO 😎"',
+    '"I love to code, but I also love to play games"',
+    '"BIg fan of all things basketball 🏀"',
+  ];
