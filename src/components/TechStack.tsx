79c0b5df51ae8c5ef44f93dd772133cbcf929a53
Kush Patel <kushpatel76@yahoo.com>
2024-12-29 02:49:48 -0500

    Add infra and basics for scrolling tech bar

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -4,6 +4,7 @@ import { useTypewriter, Cursor } from 'react-simple-typewriter';
 import { quotes } from '../helpers/quotes';
 import { personalQuotes } from '../helpers/kPatelFacts';
 import test from 'node:test';
+import { images } from '../helpers/techImageExporter';
 
 const getRandomQuote = () => {
   return quotes[Math.floor(Math.random() * quotes.length)];
@@ -35,19 +36,31 @@ const TechStack = () => {
 
   return (
     <div id="Tech Stack" className="order-5 md:order-5 col-span-1 md:col-span-3 p-4 w-full rounded-lg bg-glass mt-4">
-
       {location.pathname === '/about' || location.pathname === '/' ? (
         <div className="text-md md:text-2xl font-bold text-center whitespace-pre-wrap break-words max-w-4xl mx-auto">
           <span>{text}</span>
           <Cursor />
         </div>
-      ) : (
-        <p className="text-md md:text-2xl font-bold text-center">
-          Tech Stack
-        </p>
-      )}
+      ) : location.pathname === '/projects' ? (
+        <div>
+          <div className="flex animate-scroll space-x-4">
+            {images.map(({ src, name }, index) => (
+              <img key={index} src={src} alt={name} className="h-20 w-20 object-contain" />
+            ))}
+            {images.map(({ src, name }, index) => (
+              <img key={index + images.length} src={src} alt={name} className="h-20 w-20 object-contain" />
+            ))}
+          </div>
+        </div>
+      ) : <div>Tech Stack</div>}
     </div>
   );
-}
+};
 
 export default TechStack;
+
+/*          <div className="flex overflow-x-scroll space-x-4">
+            {techStackImages.map((image, index) => (
+              <img key={index} src={image} alt={`Tech Stack ${index}`} className="h-20 w-20 object-contain" />
+            ))}
+          </div> */
---
+++ src/helpers/techImageExporter.ts
@@ -0,0 +1,42 @@
+export const images = [
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png',
+        name: 'C'
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg',
+        name: 'CSS'
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg',
+        name: 'HTML'
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/en/3/30/Java_programming_language_logo.svg',
+        name: 'Java'
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg',
+        name: 'Node.js'
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/3/31/Python-logo.png',
+        name: 'Python'
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+        name: 'React'
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/4/4c/Typescript_logo_2020.svg',
+        name: 'TypeScript'
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Haskell-Logo.svg',
+        name: 'Haskell'
+    },
+    {
+        src: 'https://upload.wikimedia.org/wikipedia/commons/3/3b/Javascript_Logo.png',
+        name: 'JavaScript'
+    }
+];
