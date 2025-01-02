4d8bb3a8c969fec8efa9fa94755c987779cd4ac7
Kush Patel <kushpatel76@yahoo.com>
2025-01-02 02:27:23 -0500

    Fix footer to not cause the screen to scroll

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    /*
-    <footer className="bg-glass w-full p-4 text-center text-white">
+    <footer className="bg-glass w-full p-4 text-center text-white md:fixed md:bottom-0 md:left-0">
       <p className="text-sm md:text-sm">
         &copy; {new Date().getFullYear()} Kush Patel. All rights reserved.
       </p>
     </footer>
-    */
-   <div></div>
   );
 }
 
