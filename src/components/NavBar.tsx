fb45282e39e9db98b485ae5f188cec0a6afec0b6
Kush Patel <kushpatel76@yahoo.com>
2025-01-02 21:54:04 -0500

    Add new Home File and update navbar/app to include it

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,6 +14,9 @@ const NavBar = () => {
   return (
     <nav className="order-2 md:order-3 h-auto flex flex-col justify-center items-center space-y-4 rounded-lg p-8 bg-glass">
       <div className="flex flex-col items-center space-y-4">
+        <Link to="/" className={`${getLinkClass('/')} text-white font-bold font-sans text-2xl`}>
+          Home
+        </Link>
         <Link to="/about" className={`${getLinkClass('/about')} text-white font-bold font-sans text-2xl`}>
           About Me
         </Link>
